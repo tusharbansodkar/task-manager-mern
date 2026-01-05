7849085ec8ad069d6ee02b1e3e4cbbc7a4b44744
tushar@vivobook <tusharbansodkar@gmail.com>
2026-01-05 21:49:10 +0530

    added api to upload images

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -8,11 +8,25 @@ const {
 
 const { protect, adminOnly } = require("../middlewares/authMiddleware");
 
+const upload = require("../middlewares/uploadMiddleware");
+
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", login);
 router.get("/profile", protect, getUserProfile);
 router.put("/profile", protect, updateUserProfile);
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file updaded" });
+  }
+
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+
+  res.status(200).json({ imageUrl });
+});
 
 module.exports = router;
