9d71d633adcc3d711ca90b859b5f18689435f2f0
tushar@vivobook <tusharbansodkar@gmail.com>
2026-01-20 17:40:01 +0530

    refactored routes for soft delete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 
 const getUsers = async (req, res) => {
   try {
-    const users = await User.find().select("-password");
+    const users = await User.find({ isDeleted: false }).select("-password");
     res.json(users);
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
@@ -12,6 +12,17 @@ const getUsers = async (req, res) => {
 
 const getUserById = async (req, res) => {
   try {
+    const { id } = req.params;
+
+    const user = await User.findOne({ _id: id, isDeleted: false }).select(
+      "-password",
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
@@ -19,6 +30,27 @@ const getUserById = async (req, res) => {
 
 const deleteUser = async (req, res) => {
   try {
+    const { id } = req.params;
+    const loggedInUser = req.user;
+
+    // Prevent admin from deleting their own account
+    if (loggedInUser._id.toString() === id) {
+      return res
+        .status(400)
+        .json({ message: "Admin users cannot delete their own account." });
+    }
+
+    const user = await User.findByIdAndUpdate(
+      { _id: id, isDeleted: false },
+      { isDeleted: true },
+      { new: true },
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully." });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,7 +10,7 @@ const {
 // User management routes
 
 router.get("/", protect, adminOnly, getUsers); // Get all users (admin only)
-router.get("/:id", protect, adminOnly, getUserById); // Get user specific role
+router.get("/:id", protect, getUserById); // Get user specific role
 router.delete("/:id", protect, adminOnly, deleteUser); // Delete user (admin only)
 
 module.exports = router;
